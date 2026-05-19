3c560d076c9a4e84b71f6cb4d7e5894dd444074a
Pankyes <praisemandik8@gmail.com>
2026-05-19 17:33:06 +0100

    Arigato

--- Day2/Java/Level 3/TaskNine.java
+++ Day2/Java/Level 3/TaskNine.java
@@ -3,7 +3,10 @@ public class TaskNine{
     public static void main(String[]args){
         for(int i= 1; i>=6; i++){
             System.out.print("*");
-        }
+        
+            for(int j = 1; j<i; j++) {
             System.out.println("*");
-    }    
-}            
+     }    
+    } 
+   }
+}           
