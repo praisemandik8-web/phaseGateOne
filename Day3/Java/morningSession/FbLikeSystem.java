619b72f3311a3ae10f1775fcad6955a7db539b35
Pankyes <praisemandik8@gmail.com>
2026-05-20 12:25:28 +0100

    omo

---
+++ Day3/Java/morningSession/FbLikeSystem.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class FbLikeSystem{
+
+
+    public static  isNull(String [] names){
+        return null;
+    }
+    
+    public static void main(String[]args){
+    Scanner inputCollector = new Scanner(System.in);
+    String []names = new String[10];
+    int count = 0;
+    for(; count<names.length; count++){   
+        System.out.println("ENter names");
+        names[count] = inputCollector.nextLine();
+    return names[count]
+            
+        
+        }
+    }
+
+    public static  isNull(String [] names){
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
