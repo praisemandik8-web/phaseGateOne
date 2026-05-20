988874f73dba40bb89ee8cae1b372805d1e42698
Pankyes <praisemandik8@gmail.com>
2026-05-20 12:31:05 +0100

    omo

---
+++ Day3/Java/morningSession/FbLikeSystemTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FbLikeSystemTest{
+
+    @Test
+    public void testThatArrayIsEmptyAndNull(){
+    
+    assertNull(FbLikeSystem.isNull);
+    }
